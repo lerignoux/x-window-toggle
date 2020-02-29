556d9a09eb3a659d7fc1579de40cb996e07f1de6
Laurent Erignoux <lerignoux@gmail.com>
2020-02-29 20:19:04 +0800

    Ensuring window is focused automatically

--- toggle.sh
+++ toggle.sh
@@ -3,11 +3,14 @@
 # Toggle a window
 window="$1"
 # awk remove the window long name and avoid that your browser, shell, ... searching for instance `alacritty` collides with the software
-desktop="$(wmctrl -lx | awk '{print $1, $2, $3}' | grep -i "$window" | sed -n 's/0x.*[[:space:]]\([[:digit:]]\)\s.*/\1/p')"
+window_info="$(wmctrl -lx | awk '{print $1, $2, $3}' | grep -i "$window")"
+window_desktop="$(echo "$window_info" | sed -n 's/0x.*[[:space:]]\([[:digit:]]\)\s.*/\1/p')"
+window_id="$(echo "$window_info" | awk '{print $1}')"
 # echo "Switching $window from $desktop"
-desktop=$((1 - $desktop))
-eval "$(wmctrl -x -r $window -t $desktop)"
-if [ "$desktop" -eq "0" ]
+window_desktop=$((1 - $window_desktop))
+eval "$(wmctrl -x -r $window -t $window_desktop)"
+if [ "$window_desktop" -eq "0" ]
 then
-  eval "$(wmctrl -x -a $window)"
+  eval "$(wmctrl -R $window)"
+  eval "$(xdotool windowfocus $window_id)"
 fi
