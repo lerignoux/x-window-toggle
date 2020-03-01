3944ffcac97b1e9cba926fadfb4d114cea76062a
Laurent Erignoux <lerignoux@gmail.com>
2020-03-01 15:20:41 +0800

    Ensuring window is raised as well as focused

--- toggle.sh
+++ toggle.sh
@@ -12,5 +12,6 @@ eval "$(wmctrl -x -r $window -t $window_desktop)"
 if [ "$window_desktop" -eq "0" ]
 then
   eval "$(wmctrl -R $window)"
+  eval "$(xdotool windowraise $window_id)"
   eval "$(xdotool windowfocus $window_id)"
 fi
